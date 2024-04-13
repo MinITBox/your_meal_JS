bd48921ff5843f7fb3bc18bfaf5e9ec209c59f8d
MinITBag <166734704+MinITBag@users.noreply.github.com>
2024-04-13 21:21:44 +0300

    Update const.js

--- js/const.js
+++ js/const.js
@@ -1,2 +1,2 @@
-export const API_URL = 'http://giant-freezing-pentagon.glitch.me';
+export const API_URL = 'https://giant-freezing-pentagon.glitch.me';
 export const PREFIX_PRODUCT = '/api/product';
